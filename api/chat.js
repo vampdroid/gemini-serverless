f4b00610bd70f69658ae2db21ff11a0d00aa1d76
vampdroid <89890815+vampyash@users.noreply.github.com>
2025-05-31 00:31:56 +0530

    Add system instructions

--- api/chat.js
+++ api/chat.js
@@ -21,13 +21,29 @@ export default async function handler(req, res) {
 
 	const { prompt } = req.body || "Default prompt";
 	const apiKey = process.env.GEMINI_API_KEY;
+	const systemInstruction = process.env.GEMINI_SYSTEM_INSTRUCTION;
 
 	const geminiRes = await fetch(
 		`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
 		{
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ contents: [{ parts: [{ text: prompt }] }] })
+			body: JSON.stringify(
+				{
+					contents: [
+						{ parts: 
+							[{ text: prompt }] 
+						}
+					],
+					systemInstruction: {
+						parts: [
+							{
+							text: systemInstruction,
+							}
+						]
+					},
+				}
+			),
 		}
 	);
 
